893dc36139e5056a3a61397f3acd87f4a4040f76
Isak Anderson <94191077+isakanderson-official@users.noreply.github.com>
2024-07-30 09:30:23 -0500

    Add todo item in memory

--- main.go
+++ main.go
@@ -24,8 +24,21 @@ func main() {
 		tmpl.Execute(w, data)
 	 }
 
+	 addTodoHandler := func(w http.ResponseWriter, r *http.Request){
+
+		message := r.PostFormValue("message")
+
+		templ := template.Must(template.ParseFiles("index.html"))
+
+		todo := Todo{Id: len(data["Todos"]) + 1, Message: message}
+
+		data["Todos"] = append(data["Todos"], todo)
+
+		templ.ExecuteTemplate(w, "todo-list-element",todo)
+	 }
+
 
 	http.HandleFunc("/", todosHandler)
-
+	http.HandleFunc("/add-todo", addTodoHandler)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
